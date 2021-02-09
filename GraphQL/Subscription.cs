684b6eb8800eadbbd1aec45a52361b1967f30426
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2021-02-09 13:57:53 +0000

    Improved documentation for subscription

--- GraphQL/Subscription.cs
+++ GraphQL/Subscription.cs
@@ -4,10 +4,20 @@ using HotChocolate.Types;
 
 namespace CommanderGQL.GraphQL
 {
+    /// <summary>
+    /// Represents the subscriptions available.
+    /// </summary>
+    [GraphQLDescription("Represents the queries available.")]
     public class Subscription
     {
+        /// <summary>
+        /// The subscription for added <see cref="Platform"/>.
+        /// </summary>
+        /// <param name="platform">The <see cref="Platform"/>.</param>
+        /// <returns>The added <see cref="Platform"/>.</returns>
         [Subscribe]
         [Topic]
+        [GraphQLDescription("The subscription for added platform.")]
         public Platform OnPlatformAdded([EventMessage] Platform platform)
         {
             return platform;
